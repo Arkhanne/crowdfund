99cdea9eb9ed507ff74f0a28f5a3bb5a3db4281b
Edu Benito <eduardbenito@icloud.com>
2018-10-05 18:41:20 +0200

    list projects

---
+++ crowdfund.rb
@@ -0,0 +1,7 @@
+project1 = "ABC"
+project2 = "LMN"
+project3 = "XYZ"
+funds1 = 1000
+
+puts "Project #{project1} has $#{funds1} in funding."
+puts "Projects:\n\t#{project1}\n\t#{project2}\n\t#{project3}"
