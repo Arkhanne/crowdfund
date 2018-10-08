a275300c37ed2b4ccac74ef958d3263fb439eb64
Edu Benito <eduardbenito@icloud.com>
2018-10-08 18:10:36 +0200

    use methods

--- crowdfund.rb
+++ crowdfund.rb
@@ -4,9 +4,23 @@ project3 = "XYZ"
 funds1 = 1000
 funds2 = 700
 funds3 = 4000
+project_column_name = "Project"
+funds_column_name = "Funds"
+offset_columns = 30
 
-puts "Project".ljust(30) + "Funds"
-puts "".ljust(35, "-")
-puts "#{project1.ljust(34 - funds1.to_s.length, ".")}#{funds1}$"
-puts "#{project2.ljust(34 - funds2.to_s.length, ".")}#{funds2}$"
-puts "#{project3.ljust(34 - funds3.to_s.length, ".")}#{funds3}$"
+def list_title(column1_name, column2_name, offset)
+  total_offset = offset + column2_name.length
+
+  puts column1_name.ljust(offset) + column2_name
+  puts "".ljust(offset + column2_name.length, "-")
+  total_offset
+end
+
+def project_list(project, funds=0, offset)
+  "#{project.ljust(offset - 1 - funds.to_s.length, ".")}#{funds}$"  
+end
+
+offset = list_title(project_column_name, funds_column_name, offset_columns)
+puts project_list(project1, funds1, offset)
+puts project_list(project2, funds2, offset)
+puts project_list(project3, funds3, offset)
