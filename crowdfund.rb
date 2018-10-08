ce690396a06e54d232f4d87878b0bebcfa86a003
Edu Benito <eduardbenito@icloud.com>
2018-10-08 12:23:52 +0200

    show projects and founds list

--- crowdfund.rb
+++ crowdfund.rb
@@ -2,6 +2,11 @@ project1 = "ABC"
 project2 = "LMN"
 project3 = "XYZ"
 funds1 = 1000
+funds2 = 700
+funds3 = 4000
 
-puts "Project #{project1} has $#{funds1} in funding."
-puts "Projects:\n\t#{project1}\n\t#{project2}\n\t#{project3}"
+puts "Project".ljust(30) + "Funds"
+puts "".ljust(35, "-")
+puts "#{project1.ljust(34 - funds1.to_s.length, ".")}#{funds1}$"
+puts "#{project2.ljust(34 - funds2.to_s.length, ".")}#{funds2}$"
+puts "#{project3.ljust(34 - funds3.to_s.length, ".")}#{funds3}$"
