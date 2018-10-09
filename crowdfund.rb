73f3db5854baf50fe242d45eb3e2db46af59f4e2
Edu Benito <eduardbenito@icloud.com>
2018-10-09 16:12:33 +0200

    add class attributes

--- crowdfund.rb
+++ crowdfund.rb
@@ -1,22 +1,25 @@
 class Project
-  def initialize(name, initialAmount=0, targetAmount)
+  attr_reader :funding, :targetFunding
+  attr_accessor :name
+
+  def initialize(name, initialFunding=0, targetFunding)
     @name = name
-    @amount = initialAmount
-    @targetAmount = targetAmount
+    @funding = initialFunding
+    @targetFunding = targetFunding
   end
 
   def addFunds(amount)
-    @amount += amount
+    @funding += amount
     puts "Project #{@name} got some funds!"
   end
 
   def removeFunds(amount)
-    @amount -= amount
+    @funding -= amount
     puts "Project #{@name} lost some funds!"
   end
 
   def to_s
-    "Project #{@name} has $#{@amount} in funding towards a goal og $#{@targetAmount}."
+    "Project #{@name} has $#{@funding} in funding towards a goal og $#{@targetFunding}."
   end
 end
 
@@ -29,3 +32,4 @@ project1.removeFunds(100)
 project2.addFunds(10)
 puts project1
 puts project2
+puts "Project #{project1.name} has a total total funding still needed of $#{project1.targetFunding - project1.funding}"
